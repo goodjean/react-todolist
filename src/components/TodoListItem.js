a93c5be801a554b7e7da09f021b3383e8c34ced4
goodjean <goodjean1@naver.com>
2022-04-01 00:56:52 +0900

    수정버튼, 수정확인, 수정취소 버튼 생성

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,12 +1,38 @@
+import { useState } from "react";
+
 function TodoListItem({ todo, todolist, setTodolist }) {
+  const [newTodo, setNewTodo] = useState(todo.text);
+  const [state, setState] = useState(false);
   const btnDelete = (key) => {
     setTodolist(todolist.filter((todo) => todo.id !== key));
   };
+
+  const modify = () => {};
+  const modifyCancel = () => {
+    setState(false);
+  };
   return (
-    <>
-      <div>{todo.text}</div>
-      <button onClick={() => btnDelete(todo.id)}>삭제</button>
-    </>
+    <div>
+      {state ? (
+        <>
+          <input
+            type="text"
+            value={newTodo}
+            onChange={({ target }) => {
+              setNewTodo(target.value);
+            }}
+          ></input>
+          <button onClick={() => modify()}>수정확인</button>
+          <button onClick={() => modifyCancel()}>수정취소</button>
+        </>
+      ) : (
+        <>
+          <div>{todo.text}</div>
+          <button onClick={() => setState(true)}>수정</button>
+          <button onClick={() => btnDelete(todo.id)}>삭제</button>
+        </>
+      )}
+    </div>
   );
 }
 
